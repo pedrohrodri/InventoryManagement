26e2082bf0c5fbaa9c45377a70c58393f7627701
Pedro - Dev Java/Kotlin <phr-pedro@hotmail.com>
2023-12-28 15:01:51 -0300

    Início do projeto de gerenciamento de estoque.

---
+++ src/test/java/br/com/pedro/inventorymanagement/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.inventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
